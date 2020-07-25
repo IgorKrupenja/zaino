895aeb34f7f2c1fd0033557ad66509c42445dccf
Igor Krupenja <igor.krupenja@gmail.com>
2020-07-25 15:06:26 +0300

    Fix SCSS hot reload with development server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -27,8 +27,8 @@ module.exports = env => {
     module: {
       rules: [
         {
-          loader: 'babel-loader',
           test: /\.jsx?$/,
+          loader: 'babel-loader',
           exclude: /node_modules/,
           resolve: {
             extensions: ['.js', '.jsx'],
@@ -36,22 +36,27 @@ module.exports = env => {
         },
         {
           test: /\.s?css$/,
-          use: ExtractTextPlugin.extract({
-            use: [
-              {
-                loader: 'css-loader',
-                options: {
-                  sourceMap: true,
-                },
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                // hot module reload for SCSS in development
+                hmr: process.env.NODE_ENV === 'development',
               },
-              {
-                loader: 'sass-loader',
-                options: {
-                  sourceMap: true,
-                },
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
               },
-            ],
-          }),
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
         },
         {
           test: /\.(png|jp(e*)g|svg|gif)$/,
@@ -71,7 +76,6 @@ module.exports = env => {
       ],
     },
     plugins: [
-      new ExtractTextPlugin('styles.css'),
       new webpack.DefinePlugin({
         'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
         'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
@@ -88,6 +92,7 @@ module.exports = env => {
         template: 'src/index.html',
         favicon: './src/images/favicon.png',
       }),
+      new MiniCssExtractPlugin(),
     ],
     devtool: env === 'production' ? 'source-map' : 'inline-source-map',
     devServer: {
