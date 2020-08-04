9f72b72ace73d3125d980b1fbdddff1eeb458c57
Igor Krupenja <igor.krupenja@gmail.com>
2020-08-04 23:42:31 +0300

    Add support for React Fast Refresh

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -11,94 +12,101 @@ if (process.env.NODE_ENV === 'test') {
   require('dotenv').config({ path: '.env.development' });
 }
 
-module.exports = env => {
-  return {
-    entry: ['./src/app.tsx'],
-    output: {
-      path: path.join(__dirname, 'dist'),
-      filename: 'bundle.js',
-      // makes dev server work with nested paths
-      publicPath: '/',
-    },
-    watchOptions: {
-      // needed for WebPack to attempt recompiling periodically after a failure
-      poll: 1000,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(t|j)sx?$/,
-          use: { loader: 'ts-loader' },
-          exclude: /node_modules/,
-          resolve: {
-            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  entry: ['./src/app.tsx'],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    // makes dev server work with nested paths
+    publicPath: '/',
+  },
+  watchOptions: {
+    // needed for WebPack to attempt recompiling periodically after a failure
+    poll: 1000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+        use: [
+          isDevelopment && {
+            loader: 'babel-loader',
+            options: { plugins: ['react-refresh/babel'] },
           },
-        },
-        {
-          test: /\.s?css$/,
-          use: [
-            {
-              loader: MiniCssExtractPlugin.loader,
-              options: {
-                // hot module reload for SCSS in development
-                hmr: process.env.NODE_ENV === 'development',
-              },
+          'ts-loader',
+        ],
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // hot module reload for SCSS in development
+              hmr: isDevelopment,
             },
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: true,
-              },
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
             },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true,
-              },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
             },
-          ],
-        },
-        {
-          test: /\.(png|jp(e*)g|svg|gif)$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                // needed to support dynamic image import with require
-                esModule: false,
-                // output images with proper names to dist/images dir
-                context: path.resolve(__dirname, 'src'),
-                name: '[path][name].[ext]',
-              },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              // needed to support dynamic image import with require
+              esModule: false,
+              // output images with proper names to dist/images dir
+              context: path.resolve(__dirname, 'src'),
+              name: '[path][name].[ext]',
             },
-          ],
-        },
-      ],
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
-        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(
-          process.env.FIREBASE_MESSAGING_SENDER_ID
-        ),
-        'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
-        'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID),
-      }),
-      new HtmlWebpackPlugin({
-        template: 'src/index.html',
-        favicon: './src/images/favicon.png',
-      }),
-      new MiniCssExtractPlugin(),
+          },
+        ],
+      },
     ],
-    devtool: env === 'production' ? 'source-map' : 'inline-source-map',
-    devServer: {
-      contentBase: path.join(__dirname, 'dist'),
-      historyApiFallback: true,
-      hot: true,
-    },
-  };
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+      'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+      'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+      'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+      'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+      'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(
+        process.env.FIREBASE_MESSAGING_SENDER_ID
+      ),
+      'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
+      'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID),
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      favicon: './src/images/favicon.png',
+    }),
+    new MiniCssExtractPlugin(),
+    isDevelopment && new ReactRefreshWebpackPlugin(),
+  ],
+  devtool: isDevelopment ? 'inline-source-map' : 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    historyApiFallback: true,
+    hot: true,
+  },
 };
