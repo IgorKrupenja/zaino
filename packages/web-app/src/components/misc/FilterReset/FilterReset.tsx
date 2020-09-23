ea87a2ff320b7dffd5ce610ed6f5dbf0ddc43144
Igor Krupenja <igor.krupenja@gmail.com>
2020-09-23 21:52:23 +0300

    Fix #351

--- packages/web-app/src/components/misc/FilterReset/FilterReset.tsx
+++ packages/web-app/src/components/misc/FilterReset/FilterReset.tsx
@@ -10,8 +10,19 @@ type FilterResetProps = {
   children: ReactNode;
 };
 
+function isFilteringItems(filters: ItemFilters | LabelFilters): filters is ItemFilters {
+  return (filters as ItemFilters).labels !== undefined;
+}
+
 export const FilterReset = ({ filters, initialFilters, children }: FilterResetProps) => {
-  const isFiltering = !shallowEqual(filters, initialFilters);
+  let isFiltering = false;
+
+  isFiltering =
+    !shallowEqual(filters, initialFilters) &&
+    // if dealing with item filters (and not label filters),
+    // also check that filters by label are set;
+    // shallowEqual does not check for that correctly
+    (isFilteringItems(filters) ? !(filters.labels.length === 0) : true);
 
   return isFiltering ? <div>{children}</div> : <></>;
 };
