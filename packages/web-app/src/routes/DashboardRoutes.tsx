750684d4e34473b557aba5f173787d19044de246
Igor Krupenja <igor.krupenja@gmail.com>
2020-09-27 14:18:23 +0300

    Update packages and repo config

--- packages/web-app/src/routes/DashboardRoutes.tsx
+++ packages/web-app/src/routes/DashboardRoutes.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Redirect, Route } from 'react-router-dom';
+import { Redirect, Route, RouteComponentProps } from 'react-router-dom';
 import { AddItem } from '../components/ItemModal/AddItem';
 import { EditItem } from '../components/ItemModal/EditItem';
 import { RootState } from '../state/store';
 
-type Match = { params: { id: string } };
+type MatchParams = {
+  id: string;
+};
 
 /**
  * Provides routes specific to Dashboard page.
@@ -15,10 +17,12 @@ export const DashboardRoutes = () => {
 
   return (
     <>
-      <Route path="/dashboard/add" component={AddItem} />
+      <Route path="/dashboard/add">
+        <AddItem />
+      </Route>
       <Route
         path="/dashboard/edit/:id"
-        render={({ match }: { match: Match }) => {
+        render={({ match }: RouteComponentProps<MatchParams>) => {
           // show EditItem if id is valid
           if (items.find(item => item.id === match.params.id)) {
             return <EditItem />;
