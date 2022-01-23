d8f078f5b41b98a036892882d58d26681af3b7f6
Igor Krupenja <igor.krupenja@gmail.com>
2022-01-23 15:33:17 +0200

    Fix placeholder unit test

--- packages/web-app/src/components/App/App.test.tsx
+++ packages/web-app/src/components/App/App.test.tsx
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import { App } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Test', () => {
+  beforeAll(() => {
+    // @ts-ignore
+    window.matchMedia = () => ({
+      addListener: () => {},
+      removeListener: () => {},
+    });
+  });
+
+  test('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(
+      /Hiking and mountaineering equipment app for the meticulous adventurer./i
+    );
+    expect(linkElement).toBeInTheDocument();
+  });
 });
