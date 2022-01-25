929673417e2acde00991819682b72397e10e8d28
Igor Krupenja <igor.krupenja@gmail.com>
2022-01-25 23:55:08 +0200

    Add recommended extensions

--- packages/web-app/scripts/deploy.sh
+++ packages/web-app/scripts/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+# shellcheck disable=SC2046,SC2005,SC2002
 
 node scripts/build.js
 
-# shellcheck disable=SC2046,SC2005,SC2002
+# Get variables from .env file
 export $(echo $(cat .env."$1" | sed 's/#.*//g' | sed 's/\r//g' | xargs) | envsubst)
 gsutil -m cp -r "src/images/copyrighted/*" gs://"$GCP_STORAGE_BUCKET"/
 gsutil -m acl set -R -a public-read gs://"$GCP_STORAGE_BUCKET"/
