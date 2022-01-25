90db390955df0bd0c4432bd728ba203d636d7af4
Igor Krupenja <igor.krupenja@gmail.com>
2022-01-26 00:29:05 +0200

    Fix gcloud project setting

--- packages/web-app/scripts/deploy.sh
+++ packages/web-app/scripts/deploy.sh
@@ -5,7 +5,8 @@ node scripts/build.js
 
 # Get variables from .env file
 export $(echo $(cat .env."$1" | sed 's/#.*//g' | sed 's/\r//g' | xargs) | envsubst)
-gsutil -m cp -r "src/images/copyrighted/*" gs://"$GCP_STORAGE_BUCKET"/
-gsutil -m acl set -R -a public-read gs://"$GCP_STORAGE_BUCKET"/
+gcloud config set project "$REACT_APP_FIREBASE_PROJECT_ID"
+gsutil -m cp -r "src/images/copyrighted/*" gs://"$REACT_APP_FIREBASE_STORAGE_BUCKET"/
+gsutil -m acl set -R -a public-read gs://"$REACT_APP_FIREBASE_STORAGE_BUCKET"/
 
 firebase deploy -P "$1" --only hosting
