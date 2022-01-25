8e469f2f951d830b4f1dc28a072c8e3b71b20eb2
Igor Krupenja <igor.krupenja@gmail.com>
2022-01-25 22:34:41 +0200

    Fix images deploy

--- packages/web-app/scripts/deploy.sh
+++ packages/web-app/scripts/deploy.sh
@@ -2,7 +2,9 @@
 
 node scripts/build.js
 
-export "$(echo $(cat .env.development | sed 's/#.*//g' | sed 's/\r//g' | xargs) | envsubst)"
+# shellcheck disable=SC2046,SC2005,SC2002
+export $(echo $(cat .env."$1" | sed 's/#.*//g' | sed 's/\r//g' | xargs) | envsubst)
 gsutil -m cp -r "src/images/copyrighted/*" gs://"$GCP_STORAGE_BUCKET"/
+gsutil -m acl set -R -a public-read gs://"$GCP_STORAGE_BUCKET"/
 
 firebase deploy -P "$1" --only hosting
