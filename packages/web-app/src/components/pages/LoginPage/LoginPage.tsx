9855e2b9ff52c049465decbcdaec3edbd2866d21
Igor Krupenja <igor.krupenja@gmail.com>
2022-02-05 18:46:10 +0200

    Clean up

--- packages/web-app/src/components/pages/LoginPage/LoginPage.tsx
+++ packages/web-app/src/components/pages/LoginPage/LoginPage.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
 import GoogleButton from 'react-google-button';
 import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
-import { auth, firebase } from '../../../firebase/firebase';
+import { firebase } from '../../../firebase/firebase';
 import { useTitle } from '../../../hooks/useTitle';
 import { ReactComponent as GithubIcon } from '../../../images/icons/github.svg';
 import { loadUserData } from '../../../state/slices/demoDataSlice';
@@ -14,32 +13,17 @@ import './style.scss';
 
 export const LoginPage = () => {
   const dispatch = useDispatch();
-  // const [isFirstUpdate, setIsFirstUpdate] = useState(true);
-  const [isPageLoading, setIsPageLoading] = useState(false);
-  const [user, loading, error] = useAuthState(auth);
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
   useTitle('Zaino');
 
-  // // Create a currentUser state
-  const [currentUser, setCurrentUser] = useState<string | null>(null);
-
-  // useEffect(() => console.log(asciiLogo));
-
   useEffect(() => {
-    // const handleLogin = async (user: User) => {
-    //   const credential = await firebase.auth().getRedirectResult();
-    //   // Using store.dispatch as useDispatch cannot be used outside of functional components
-    //   dispatch(login({ user, isNew: credential.additionalUserInfo?.isNewUser }));
-    // };
-    console.log('LoginPage.useEffect');
-    setIsPageLoading(true);
+    setIsLoading(true);
 
     if (firebase) {
       firebase.auth().onAuthStateChanged(async (user) => {
-        console.log('LoginPage.useEffect.onAuthStateChanged');
         if (user) {
-          setIsPageLoading(true);
-          console.log('LoginPage.useEffect.onAuthStateChanged.authUser', user);
+          setIsLoading(true);
 
           // User is signed in
           const credential = await firebase.auth().getRedirectResult();
@@ -51,66 +35,13 @@ export const LoginPage = () => {
           // todo this defs to dashboard? or maybe in component
           console.log(asciiLogo);
         } else {
-          setIsPageLoading(false);
+          setIsLoading(false);
         }
       });
     }
   }, [dispatch, navigate]);
 
-  // useEffect(() => {
-  //   let mounted = true;
-
-  //   console.log('LoginPage.useEffect');
-
-  //   // todo move and rename. setUserData?
-  //   const handleLogin = async (user: User) => {
-  //     const credential = await firebase.auth().getRedirectResult();
-  //     // Using store.dispatch as useDispatch cannot be used outside of functional components
-  //     dispatch(login({ user, isNew: credential.additionalUserInfo?.isNewUser }));
-  //   };
-
-  //   // const handleLogin = async (user: firebase.User) => {
-  //   //   const credential = await firebase.auth().getRedirectResult();
-  //   //   // Using store.dispatch as useDispatch cannot be used outside of functional components
-  //   //   dispatch(login({ user, isNew: credential.additionalUserInfo?.isNewUser }));
-  //   // };
-  //   // if (isFirstUpdate) {
-  //   //   console.log('first update');
-  //   //   setIsFirstUpdate(false);
-  //   //   return;
-  //   // }
-
-  //   // if (firstUpdate.current) {
-  //   //   console.log('first update');
-  //   //   firstUpdate.current = false;
-  //   //   return;
-  //   // }
-
-  //   // if (loading) {
-  //   //   console.log('loading');
-  //   //   setIsPageLoading(true);
-
-  //   //   // maybe trigger a loading screen
-  //   //   return;
-  //   // }
-  //   if (user) {
-  //     console.log('got user', user);
-  //     if (mounted) setIsPageLoading(true);
-  //     void handleLogin(user);
-  //     // return () => {
-  //     //   setState({}); // This worked for me
-  //     // };
-  //     navigate('/dashboard');
-  //     // setIsPageLoading(false);
-  //     return;
-  //   }
-  //   return () => {
-  //     mounted = false;
-  //   };
-  //   // todo review
-  // });
-
-  return isPageLoading ? (
+  return isLoading ? (
     <Loader />
   ) : (
     <main className="login-page">
@@ -119,7 +50,7 @@ export const LoginPage = () => {
         Hiking and mountaineering equipment app for the meticulous adventurer.
       </p>
       <GoogleButton
-        // fix width, cannot set in SCSS as component overwrites class styles with inline styles
+        // Fix width, cannot set in SCSS as component overwrites class styles with inline styles
         style={{ width: '20rem', textAlign: 'start' }}
         type="light"
         onClick={() => dispatch(login_TEMP_MOVE_TO_SERVICE())}
