a8c722b73b98bfe336b2949c9891b7e4b186337e
Igor Krupenja <igor.krupenja@gmail.com>
2022-02-05 22:19:33 +0200

    Create auth component

--- packages/web-app/src/components/App/App.tsx
+++ packages/web-app/src/components/App/App.tsx
@@ -1,80 +1,24 @@
-import { Category, Item, Label } from '@zaino/shared';
-import {
-  getAdditionalUserInfo,
-  getAuth,
-  getRedirectResult,
-  onAuthStateChanged,
-  User,
-} from 'firebase/auth';
-import { StrictMode, useEffect, useState } from 'react';
+import { StrictMode } from 'react';
 import Media from 'react-media';
-import { batch, Provider } from 'react-redux';
-import { db, getObjectsFromSnapshots } from '../../firebase';
+import { Provider } from 'react-redux';
 import AppRouter from '../../routes/AppRouter';
-import { addCategories } from '../../state/slices/categoriesSlice';
-import { addItems } from '../../state/slices/itemsSlice';
-import { addLabels } from '../../state/slices/labelsSlice';
-import { login, logout } from '../../state/slices/userSlice';
 import { store } from '../../state/store';
-import { asciiLogo } from '../../utils';
-import { Loader } from '../Common/Misc/Loader';
+import { AuthStateHandler } from '../AuthStateHandler';
 import { MobilePlaceholder } from '../pages/MobilePlaceholder';
 
 export const App = () => {
-  const [isLoading, setIsLoading] = useState(false);
-
-  const loadUserData = async (uid: string) => {
-    const snapshots = await Promise.all([
-      db.collection(`users/${uid}/items`).get(),
-      db.collection(`users/${uid}/labels`).get(),
-      db.collection(`users/${uid}/categories`).get(),
-    ]);
-
-    const [items, labels, categories] = getObjectsFromSnapshots(snapshots) as [
-      Item[],
-      Label[],
-      Category[]
-    ];
-
-    batch(() => {
-      store.dispatch(addItems(items));
-      store.dispatch(addLabels({ labels, items }));
-      store.dispatch(addCategories({ categories, items }));
-    });
-  };
-
-  useEffect(() => {
-    setIsLoading(true);
-    const auth = getAuth();
-
-    const onAuthStateChangeHandler = async (user: User | null) => {
-      if (user) {
-        setIsLoading(true);
-
-        const credential = await getRedirectResult(auth);
-        await store.dispatch(
-          login({ user, isNew: credential ? getAdditionalUserInfo(credential)?.isNewUser : false })
-        );
-        await loadUserData(user.uid);
-
-        console.log(asciiLogo);
-
-        setIsLoading(false);
-      } else {
-        await store.dispatch(logout());
-        setIsLoading(false);
-      }
-    };
-
-    onAuthStateChanged(auth, (user) => void onAuthStateChangeHandler(user));
-  }, []);
-
   return (
     <StrictMode>
       <Provider store={store}>
         <Media queries={{ small: { maxWidth: 599 } }}>
           {(matches) =>
-            matches.small ? <MobilePlaceholder /> : isLoading ? <Loader /> : <AppRouter />
+            matches.small ? (
+              <MobilePlaceholder />
+            ) : (
+              <AuthStateHandler>
+                <AppRouter />
+              </AuthStateHandler>
+            )
           }
         </Media>
       </Provider>
--- packages/web-app/src/components/AuthStateHandler/AuthStateHandler.tsx
+++ packages/web-app/src/components/AuthStateHandler/AuthStateHandler.tsx
@@ -1,1 +1,74 @@
-export const AuthStateHandler = () => {};
+import { Category, Item, Label } from '@zaino/shared';
+import {
+  getAdditionalUserInfo,
+  getAuth,
+  getRedirectResult,
+  onAuthStateChanged,
+  User,
+} from 'firebase/auth';
+import { ReactNode, useEffect, useState } from 'react';
+import { batch, useDispatch } from 'react-redux';
+import { db, getObjectsFromSnapshots } from '../../firebase';
+import { addCategories } from '../../state/slices/categoriesSlice';
+import { addItems } from '../../state/slices/itemsSlice';
+import { addLabels } from '../../state/slices/labelsSlice';
+import { login, logout } from '../../state/slices/userSlice';
+import { asciiLogo } from '../../utils';
+import { Loader } from '../Common/Misc/Loader';
+
+type AuthStateHandlerProps = {
+  children: ReactNode;
+};
+
+export const AuthStateHandler = ({ children }: AuthStateHandlerProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setIsLoading(true);
+    const auth = getAuth();
+
+    const loadUserData = async (uid: string) => {
+      const snapshots = await Promise.all([
+        db.collection(`users/${uid}/items`).get(),
+        db.collection(`users/${uid}/labels`).get(),
+        db.collection(`users/${uid}/categories`).get(),
+      ]);
+
+      const [items, labels, categories] = getObjectsFromSnapshots(snapshots) as [
+        Item[],
+        Label[],
+        Category[]
+      ];
+
+      batch(() => {
+        dispatch(addItems(items));
+        dispatch(addLabels({ labels, items }));
+        dispatch(addCategories({ categories, items }));
+      });
+    };
+
+    const onAuthStateChangeHandler = async (user: User | null) => {
+      if (user) {
+        setIsLoading(true);
+
+        const credential = await getRedirectResult(auth);
+        dispatch(
+          login({ user, isNew: credential ? getAdditionalUserInfo(credential)?.isNewUser : false })
+        );
+        await loadUserData(user.uid);
+
+        console.log(asciiLogo);
+
+        setIsLoading(false);
+      } else {
+        dispatch(logout());
+        setIsLoading(false);
+      }
+    };
+
+    onAuthStateChanged(auth, (user) => void onAuthStateChangeHandler(user));
+  }, [dispatch]);
+
+  return <>{isLoading ? <Loader /> : children}</>;
+};
