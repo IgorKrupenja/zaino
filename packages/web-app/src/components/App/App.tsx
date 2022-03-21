359ce92fd2c514fedd4ccda34e1a07ddf154ce78
Igor Krupenja <igor.krupenja@gmail.com>
2022-03-21 19:18:13 +0200

    Move PRovider logic to index

--- packages/web-app/src/components/App/App.tsx
+++ packages/web-app/src/components/App/App.tsx
@@ -1,8 +1,5 @@
-import { StrictMode } from 'react';
-import { Provider } from 'react-redux';
 import { useMediaQuery } from 'react-responsive';
 import { AppRouter } from '../../routes';
-import { store } from '../../state/store';
 import { AuthStateHandler } from '../AuthStateHandler';
 import { MobilePlaceholder } from '../common/Misc/MobilePlaceholder';
 
@@ -10,10 +7,6 @@ export const App = () => {
   const isSmallScreen = useMediaQuery({ query: '(max-width: 599px)' });
 
   return (
-    <StrictMode>
-      <Provider store={store}>
-        <AuthStateHandler>{isSmallScreen ? <MobilePlaceholder /> : <AppRouter />}</AuthStateHandler>
-      </Provider>
-    </StrictMode>
+    <AuthStateHandler>{isSmallScreen ? <MobilePlaceholder /> : <AppRouter />}</AuthStateHandler>
   );
 };
