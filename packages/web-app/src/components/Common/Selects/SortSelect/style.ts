4678bceb80c26a18ffba4b4036bf14e814e43703
Igor Krupenja <igor.krupenja@gmail.com>
2022-03-21 19:53:36 +0200

    Temporarily remove common components folder

--- packages/web-app/src/components/Common/Selects/SortSelect/style.ts
+++
@@ -1,8 +0,0 @@
-import { StylesConfig } from 'react-select';
-import { SelectOption } from '../SelectPopover';
-
-export const sortSelectStyles: Partial<StylesConfig<SelectOption, boolean>> = {
-  menu: () => ({
-    borderTop: 'none',
-  }),
-};
