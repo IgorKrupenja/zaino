0af878aa1db3af8cab306c983f0d2af784d6a1b0
Igor Krupenja <igor.krupenja@gmail.com>
2022-03-21 15:42:59 +0200

    Clena up comments in shared package

--- packages/shared/src/types/Category.ts
+++ packages/shared/src/types/Category.ts
@@ -2,6 +2,5 @@ import { Collection } from './Collection';
 
 export type Category = Collection & {
   imageFileName: string;
-  // default category for new item
   isDefault?: boolean;
 };
--- packages/shared/src/types/Item.ts
+++ packages/shared/src/types/Item.ts
@@ -4,12 +4,12 @@ export type Item = {
   categoryId?: string;
   quantity: number;
   packQuantity: number;
-  // allow empty string for weight — this seems better than undefined
+  // Allow empty string for weight — this seems better than undefined
   // as it prevents "A component is changing an uncontrolled input of type text to be controlled"
-  // errors in ItemForm
+  // errors in ItemForm.
   weight: number | '';
-  // as Date is not serializable and should not be stored in Redux store state,
-  // opting to use ISO format UTC string - they are also directly comparable when sorting
+  // As Date is not serializable and should not be stored in Redux store state,
+  // using ISO format UTC string - they are also directly comparable when sorting.
   addedAt: string;
   labelIds?: string[];
   notes?: string;
