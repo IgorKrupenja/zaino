e18e21ab8107350afcda1725f2dc53973a15bc32
Igor Krupenja <igor.krupenja@gmail.com>
2022-03-21 22:10:47 +0200

    Clean up List

--- packages/web-app/src/components/common/misc/List/List.tsx
+++ packages/web-app/src/components/common/misc/List/List.tsx
@@ -4,28 +4,16 @@ import './List.scss';
 
 type ListProps = {
   children: ReactNode;
-  className?: string;
   variant?: 'top-border';
 };
 
-/**
- * List component, currently used only on Labels page.
- * But potentially re-usable, if e.g. Category editor is implemented in a similar fashion.
- */
 export const List = ({ children }: ListProps) => {
   return <section className="list">{children}</section>;
 };
 
-/**
- * Sub-component to style empty list when there are no (filtered) labels.
- */
-const Empty = ({ children, className, variant }: ListProps) => {
+const Empty = ({ children, variant }: ListProps) => {
   return (
-    <section
-      className={getClassString('list--empty', { extraClassNames: className, variant: variant })}
-    >
-      {children}
-    </section>
+    <section className={getClassString('list--empty', { variant: variant })}>{children}</section>
   );
 };
 
