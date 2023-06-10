bd6fc05fb198f931922a2fa10614f4f1a51d2db0
Igor Krupenja <igor.krupenja@gmail.com>
2023-06-10 19:30:17 +0300

    Updatre Stylelint Classname Regex

--- packages/web-app/.stylelintrc.js
+++ packages/web-app/.stylelintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     'order/properties-alphabetical-order': true,
     'selector-class-pattern': [
       // BEM
-      '^[a-z]([-]?[a-z0-9]+)*(__[a-z0-9]([-]?[a-z0-9]+)*)*(--[a-z0-9]([-]?[a-z0-9]+)*)?$',
+      '^[a-z]([-]?[a-z0-9]+)*(__[a-z0-9]([-]?[a-z0-9]+)*)*(--[a-z0-9]([-]?[a-z0-9]+)*)*$',
       {
         resolveNestedSelectors: true,
       },
