55ce63b75f912fc091c4ed509ed7ce510cf45c62
Igor Krupenja <igor.krupenja@gmail.com>
2023-06-24 12:17:30 +0300

    Fixes #647 category icon padding (#661)

--- packages/web-app/src/components/common/containers/BulletRow/BulletRow.tsx
+++ packages/web-app/src/components/common/containers/BulletRow/BulletRow.tsx
@@ -14,7 +14,7 @@ type BulletRowProps = {
  * Supports wrapping so the first element on a line does not display a bullet point.
  * Based on https://stackoverflow.com/a/41080934/7405507
  */
-// TODO: Consider making it a compound component with BulletRow.Item
+// TODO: Turn into a compound component with BulletRow.Item
 export const BulletRow = ({ children, className }: BulletRowProps) => {
   return (
     <div className={getClassString('bullet-row', { extraClassNames: className })}>{children}</div>
