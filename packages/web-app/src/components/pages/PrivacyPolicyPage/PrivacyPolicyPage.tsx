40c8de800bb0f781146a19c1614129d9b50cb41e
Igor Krupenja <igor.krupenja@gmail.com>
2023-06-25 20:56:10 +0300

    Clean up PrivacyPolicyPage

--- packages/web-app/src/components/pages/PrivacyPolicyPage/PrivacyPolicyPage.tsx
+++ packages/web-app/src/components/pages/PrivacyPolicyPage/PrivacyPolicyPage.tsx
@@ -7,23 +7,6 @@ import PrivacyPolicyContent from './PrivacyPolicyContent';
 export const PrivacyPolicyPage = () => {
   useTitle('Privacy and cookie policy | Zaino');
 
-  // const PrivacyPolicyContent = lazy(async () => {
-  //   try {
-  //     // @ts-ignore
-  //     return await import('./PrivacyPolicyContent');
-  //   } catch {
-  //     console.warn(
-  //       [
-  //         'Privacy policy content not found. Please create a file named "PrivacyPolicyContent.tsx"',
-  //         ' in the "src/components/pages/PrivacyPolicy" directory.',
-  //       ].join()
-  //     );
-  //     return {
-  //       default: () => <>Privacy policy placeholder.</>,
-  //     };
-  //   }
-  // });
-
   return (
     <main className="privacy-policy">
       <p className="privacy-policy__title">Zaino</p>
